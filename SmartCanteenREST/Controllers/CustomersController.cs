f4810fede2a1c4618c27cd1386c672296a3e6488
RasmusRB <rasmusbergstroem87@gmail.com>
2020-11-27 08:38:13 +0100

    GET CustomerData from DB to REST implemented

---
+++ SmartCanteenREST/Controllers/CustomersController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ModelLib.Model;
+using SmartCanteenREST.Managers;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace SmartCanteenREST.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomersController : ControllerBase
+    {
+        CustomerManager mng = new CustomerManager();
+
+        // GET: api/<CustomersController>
+        [HttpGet]
+        public IEnumerable<Customers> Get()
+        {
+            return mng.GetCustomerData();
+        }
+
+        //// GET api/<CustomersController>/5
+        //[HttpGet("{id}")]
+        //public string Get(int id)
+        //{
+        //    return "value";
+        //}
+
+        //// POST api/<CustomersController>
+        //[HttpPost]
+        //public void Post([FromBody] string value)
+        //{
+        //}
+
+        //// PUT api/<CustomersController>/5
+        //[HttpPut("{id}")]
+        //public void Put(int id, [FromBody] string value)
+        //{
+        //}
+
+        //// DELETE api/<CustomersController>/5
+        //[HttpDelete("{id}")]
+        //public void Delete(int id)
+        //{
+        //}
+    }
+}
---
+++ SmartCanteenREST/Managers/CustomerManager.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using ModelLib.Model;
+
+namespace SmartCanteenREST.Managers
+{
+    public class CustomerManager
+    {
+        private const string connString = @"Data Source=smartcanteen-db-erver.database.windows.net;Initial Catalog=SmartCanteen-DB;User ID=smadmin;Password=Secret1234;Connect Timeout=30;Encrypt=True;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+
+        public IList<Customers> GetCustomerData()
+        {
+            List<Customers> customerData = new List<Customers>();
+
+            using (SqlConnection conn = new SqlConnection(connString))
+            {
+                conn.Open();
+
+                using (SqlCommand cmd = new SqlCommand("Select * from Customer", conn))
+                {
+                    SqlDataReader reader = cmd.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        customerData.Add(ReadNextCustomer(reader));
+                    }
+                }
+            }
+
+            return customerData;
+        }
+
+        private Customers ReadNextCustomer(SqlDataReader reader)
+        {
+            Customers customer = new Customers();
+
+            customer.Counter = reader.GetInt32(0);
+            customer.DateTime = reader.GetDateTime(1);
+
+            return customer;
+        }
+    }
+}
