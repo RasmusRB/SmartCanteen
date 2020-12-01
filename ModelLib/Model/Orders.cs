ed692c50f9936b76b569fe71055a95717743d813
RasmusRB <57805908+RasmusRB@users.noreply.github.com>
2020-12-01 10:44:45 +0100

    Order class, manager + controller

---
+++ ModelLib/Model/Orders.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ModelLib.Model
+{
+    public class Orders
+    {
+        private int _orderId;
+        private int _productCount;
+        private DateTime _orderDate;
+
+        public Orders()
+        {
+        }
+
+        public Orders(int orderId, int productCount, DateTime orderDate)
+        {
+            _orderId = orderId;
+            _productCount = productCount;
+            _orderDate = orderDate;
+        }
+
+        public int OrderId
+        {
+            get => _orderId;
+            set => _orderId = value;
+        }
+
+        public int ProductCount
+        {
+            get => _productCount;
+            set => _productCount = value;
+        }
+
+        public DateTime OrderDate
+        {
+            get => _orderDate;
+            set => _orderDate = value;
+        }
+
+        public override string ToString()
+        {
+            return $"{nameof(_orderId)}: {_orderId}, {nameof(_productCount)}: {_productCount}, {nameof(_orderDate)}: {_orderDate}";
+        }
+    }
+}
---
+++ SmartCanteenREST/Controllers/OrdersController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ModelLib.Model;
+using SmartCanteenREST.Managers;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace SmartCanteenREST.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrdersController : ControllerBase
+    {
+        OrderManager mng = new OrderManager();
+
+        // GET: api/<OrdersController>
+        [HttpGet]
+        public IEnumerable<Orders> Get()
+        {
+            return mng.GetAllOrders();
+        }
+
+        [HttpGet]
+        [Route("{date}")]
+        public Orders GetByDate(DateTime date)
+        {
+            return mng.GetOrderByDate(date);
+        }
+        //// GET api/<OrdersController>/5
+        //[HttpGet("{id}")]
+        //public string Get(int id)
+        //{
+        //    return "value";
+        //}
+
+        //// POST api/<OrdersController>
+        //[HttpPost]
+        //public void Post([FromBody] string value)
+        //{
+        //}
+
+        //// PUT api/<OrdersController>/5
+        //[HttpPut("{id}")]
+        //public void Put(int id, [FromBody] string value)
+        //{
+        //}
+
+        //// DELETE api/<OrdersController>/5
+        //[HttpDelete("{id}")]
+        //public void Delete(int id)
+        //{
+        //}
+    }
+}
