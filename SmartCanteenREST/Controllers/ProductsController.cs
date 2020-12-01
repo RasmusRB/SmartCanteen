08b09e6a415a324b149a77e5c6207a12b681161a
RasmusRB <57805908+RasmusRB@users.noreply.github.com>
2020-12-01 10:10:37 +0100

    Product Class, manager & controller.

---
+++ SmartCanteenREST/Controllers/ProductsController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ModelLib.Model;
+using SmartCanteenREST.Managers;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace SmartCanteenREST.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private ProductManager mng = new ProductManager();
+
+        // GET: api/<ProductsController>
+        [HttpGet]
+        public IEnumerable<Products> Get()
+        {
+            return mng.GetProductInfo();
+        }
+
+        //// GET api/<ProductsController>/5
+        //[HttpGet("{id}")]
+        //public string Get(int id)
+        //{
+        //    return "value";
+        //}
+
+        //// POST api/<ProductsController>
+        //[HttpPost]
+        //public void Post([FromBody] string value)
+        //{
+        //}
+
+        //// PUT api/<ProductsController>/5
+        //[HttpPut("{id}")]
+        //public void Put(int id, [FromBody] string value)
+        //{
+        //}
+
+        //// DELETE api/<ProductsController>/5
+        //[HttpDelete("{id}")]
+        //public void Delete(int id)
+        //{
+        //}
+    }
+}
---
+++ SmartCanteenREST/Managers/ProductManager.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using ModelLib.Model;
+
+namespace SmartCanteenREST.Managers
+{
+    public class ProductManager
+    {
+        private const string connString = "Data Source=smartcanteen-db-erver.database.windows.net;Initial Catalog=SmartCanteen-DB;User ID=smadmin;Password=Secret1234;Connect Timeout=30;Encrypt=True;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+        private const string GET_ALL = "Select * from Product";
+
+        /*
+         * Only GET implemented as we do not wish to
+         * POST, DELETE or UPDATE product data
+         */
+
+        // GET all product info
+        public IList<Products> GetProductInfo()
+        {
+            List<Products> productInfo = new List<Products>();
+
+            using (SqlConnection conn = new SqlConnection(connString))
+            {
+                conn.Open();
+
+                using (SqlCommand cmd = new SqlCommand(GET_ALL, conn))
+                {
+                    SqlDataReader reader = cmd.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        productInfo.Add(ReadNextProduct(reader));
+                    }
+                }
+            }
+
+            return productInfo;
+        }
+
+        // Reads the columns in the table
+        private Products ReadNextProduct(SqlDataReader reader)
+        {
+            Products product = new Products();
+
+            product.ProdId = reader.GetInt32(0);
+            product.FkId = reader.GetInt32(1);
+            product.Name = reader.GetString(2);
+            product.Price = reader.GetInt32(3);
+            product.Protein = reader.GetInt32(4);
+            product.IsHot = reader.GetBoolean(5);
+
+            return product;
+        }
+    }
+}
